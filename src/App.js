a52673a400a5ad6db2ea137c4eaf1920bd079941
cuervo-blanco <jaios.228@gmail.com>
2023-12-09 21:42:35 -0500

    began working on the structure of the app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.scss';
+import DrumMachine from './Components/DrumMachine';
+
+function App() {
+  return (
+    <div className="App">
+      <DrumMachine />
+    </div>
+  );
+}
+
+export default App;
